47175619fa5ef87572c36ec4af7185ff69f62904
Gustavo-Martins610 <gustavo981233@gmail.com>
2025-07-31 16:01:12 -0300

    Add: Adição da documentação da API

--- api-biblioteca/src/main/resources/data.sql
+++ api-biblioteca/src/main/resources/data.sql
@@ -1,11 +1,12 @@
-INSERT INTO pessoa (nome, cpf, cep, email, senha) VALUES 
-('João da Silva', '12345678900', '35501-248', 'joao@email.com', 'senha123'),
-('Maria Oliveira', '98765432100', '37500100', 'maria@email.com', 'senha456'),
-('Carlos Lima', '11122233344', '37500200', 'carlos@email.com', 'senha789'),
-('Ana Beatriz', '22233344455', '37500300', 'ana@email.com', 'senhaabc'),
-('Pedro Henrique', '33344455566', '37500400', 'pedro@email.com', 'senhaxyz');
+-- Inserir dados na tabela pessoa
+INSERT INTO pessoa (nome, cpf, cep, email, senha, numero, complemento, logradouro, bairro, cidade, estado, role) VALUES 
+('João da Silva', '123.456.789-00', '35501-248', 'joao@email.com', 'senha123', '100', 'Apto 203', 'Rua Candeias', 'Centro', 'Lavras', 'MG', 'ADMIN'),
+('Maria Oliveira', '987.654.321-00', '37500100', 'maria@email.com', 'senha456', '101', 'Apto 204', 'Rua do Sol', 'Centro', 'Lavras', 'MG', 'ADMIN'),
+('Carlos Lima', '111.222.333-44', '37500200', 'carlos@email.com', 'senha789', '102', 'Casa 5', 'Rua das Flores', 'Centro', 'Lavras', 'USER'),
+('Ana Beatriz', '222.333.444-55', '37500300', 'ana@email.com', 'senhaabc', '103', 'Apto 205', 'Rua Martins', 'São José', 'Lavras', 'USER'),
+('Pedro Henrique', '333.444.555-66', '37500400', 'pedro@email.com', 'senhaxyz', '104', 'Casa 6', 'Avenida Brasil', 'Centro', 'Lavras', 'USER');
 
-
+-- Inserir dados na tabela livro
 INSERT INTO livro (nome, autor, data_lancamento, quantidade) VALUES 
 ('Dom Casmurro', 'Machado de Assis', '1899-01-01', 5),
 ('1984', 'George Orwell', '1949-06-08', 3),
